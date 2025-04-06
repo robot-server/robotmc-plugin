fbf587154a4d01d1b35fd6d6e6c58326b1d7940d
Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>
2025-04-06 16:41:02 +0900

    First commit
    Signed-off-by: Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>

---
+++ src/main/kotlin/com/sysbot32/robotmc/plugin/RobotmcPlugin.kt
@@ -0,0 +1,14 @@
+package com.sysbot32.robotmc.plugin
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class RobotmcPlugin : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
