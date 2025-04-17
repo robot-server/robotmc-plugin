b47eb6693e724ae57a0d99a3ab73ee5a641b361b
Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>
2025-04-17 23:39:23 +0900

    DB 관련 의존성 추가
    Signed-off-by: Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -3,7 +3,11 @@ plugins {
 }
 
 dependencies {
+    api(libs.exposed.core)
+    implementation(libs.exposed.jdbc)
+    implementation("com.zaxxer:HikariCP:6.3.0")
     testImplementation(kotlin("test"))
+    runtimeOnly("com.mysql:mysql-connector-j:9.2.0")
 }
 
 tasks.test {
