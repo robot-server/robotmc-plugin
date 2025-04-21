539ae2b8f2fb65549a4ce0a557d94d56536c64f3
Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>
2025-04-21 22:45:59 +0900

    tp 코드 정리
    Signed-off-by: Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>

--- teleport/src/main/kotlin/com/sysbot32/robotmc/plugin/teleport/TeleportCommand.kt
+++ teleport/src/main/kotlin/com/sysbot32/robotmc/plugin/teleport/TeleportCommand.kt
@@ -11,7 +11,7 @@ import org.bukkit.entity.Player
 import kotlin.math.max
 
 class TeleportCommand : BukkitCommand(
-    "teleport", "다른 위치로 순간이동합니다.", "/teleport <destination: x y z>", listOf("tp", "taxi"),
+    "teleport", "다른 위치로 순간이동합니다.", "/teleport <location>", listOf("tp", "taxi"),
 ) {
     override fun execute(sender: CommandSender, commandLabel: String, args: Array<out String>): Boolean {
         if (args.size != 3) {
@@ -20,20 +20,28 @@ class TeleportCommand : BukkitCommand(
         if (sender !is Player) {
             return false
         }
-        val economy = Bukkit.getServer().servicesManager.getRegistration(Economy::class.java)?.provider ?: return false
         val destination = Location(sender.location.world, args[0].toDouble(), args[1].toDouble(), args[2].toDouble())
-        while (!destination.block.isEmpty) {
-            destination.y += 1
+        return this.teleport(sender, destination)
+    }
+
+    private fun calculatePrice(source: Location, destination: Location): Double {
+        val minPrice = 100.0
+        val distance = source.distance(destination)
+        return max(minPrice, distance).format(2).toDouble()
+    }
+
+    private fun teleport(player: Player, destination: Location): Boolean {
+        val economy = Bukkit.getServer().servicesManager.getRegistration(Economy::class.java)?.provider ?: return false
+        while (!destination.block.isEmpty) { // 생매장 방지
+            destination.y++
         }
-        val distance = sender.location.distance(destination)
-        val price = max(100.0, distance).format(2).toDouble()
-        if (!economy.has(sender, price)) {
-            sender.sendMessage("잔액이 부족합니다! 필요 금액: $price")
+        val price = this.calculatePrice(player.location, destination)
+        if (!economy.has(player, price)) {
+            player.sendMessage("잔액이 부족합니다! 필요 금액: $price")
             return false
         }
-        sender.sendMessage("${sender.location.toSimpleString()} -> ${destination.toSimpleString()}: $price")
-        economy.withdrawPlayer(sender, price)
-        sender.teleport(destination)
-        return true
+        player.sendMessage("${player.location.toSimpleString()} -> ${destination.toSimpleString()}: $price")
+        economy.withdrawPlayer(player, price)
+        return player.teleport(destination)
     }
 }
