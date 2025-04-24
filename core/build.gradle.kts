68faadce0922f94cdf8eadba275cec70478971b9
Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>
2025-04-24 20:03:21 +0900

    exposed-kotlin-datetime 의존성 추가
    Signed-off-by: Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 dependencies {
     api(libs.exposed.core)
     implementation(libs.exposed.jdbc)
+    api(libs.exposed.kotlin.datetime)
     implementation("com.zaxxer:HikariCP:6.3.0")
     testImplementation(kotlin("test"))
     runtimeOnly("com.mysql:mysql-connector-j:9.2.0")
