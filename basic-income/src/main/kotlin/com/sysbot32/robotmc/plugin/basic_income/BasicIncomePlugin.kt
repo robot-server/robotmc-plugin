2ff2e3d8d25868865ee8f53d7a2934ddac799b8e
Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>
2025-04-30 21:40:53 +0900

    basic-income:reload 추가
    Signed-off-by: Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>

--- basic-income/src/main/kotlin/com/sysbot32/robotmc/plugin/basic_income/BasicIncomePlugin.kt
+++ basic-income/src/main/kotlin/com/sysbot32/robotmc/plugin/basic_income/BasicIncomePlugin.kt
@@ -1,5 +1,6 @@
 package com.sysbot32.robotmc.plugin.basic_income
 
+import com.sysbot32.robotmc.plugin.core.ReloadCommand
 import org.bukkit.plugin.java.JavaPlugin
 import kotlin.time.Duration.Companion.minutes
 
@@ -8,5 +9,6 @@ class BasicIncomePlugin : JavaPlugin() {
         config.options().copyDefaults(true)
         saveConfig()
         BasicIncomeRunnable(this).runTaskTimerAsynchronously(this, 0, 20 * 20.minutes.inWholeSeconds)
+        server.commandMap.register(name.lowercase(), ReloadCommand(this))
     }
 }
