cca4d265a1ffb9ff251572fbc91b523f0b9109b3
Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>
2025-05-03 17:36:21 +0900

    balancetop 명령어 추가
    Signed-off-by: Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>

---
+++ economy/src/main/kotlin/com/sysbot32/robotmc/plugin/economy/BalanceTopCommand.kt
@@ -0,0 +1,46 @@
+package com.sysbot32.robotmc.plugin.economy
+
+import com.sysbot32.robotmc.plugin.core.format
+import io.github.oshai.kotlinlogging.KotlinLogging
+import net.milkbowl.vault.economy.Economy
+import org.bukkit.Bukkit
+import org.bukkit.OfflinePlayer
+import org.bukkit.command.CommandSender
+import org.bukkit.command.defaults.BukkitCommand
+import java.time.OffsetDateTime
+
+private val log = KotlinLogging.logger { }
+
+class BalanceTopCommand : BukkitCommand(
+    "balancetop", "서버 내 잔고 순위를 표시합니다.", "/balancetop", listOf("baltop"),
+) {
+    override fun execute(sender: CommandSender, commandLabel: String, args: Array<out String>): Boolean {
+        try {
+            val now = OffsetDateTime.now()
+            val players = Bukkit.getOfflinePlayers()
+            val playersWithBalance = mutableListOf<Pair<OfflinePlayer, Double>>()
+            var totalBalance = 0.0
+            Bukkit.getServicesManager().getRegistration(Economy::class.java)?.provider?.run {
+                for (player in players) {
+                    val balance = getBalance(player)
+                    playersWithBalance.add(Pair(player, balance))
+                    totalBalance += balance
+                }
+            }
+            playersWithBalance.sortBy { -it.second }
+            log.info { "playerWithBalance: $playersWithBalance" }
+            log.info { "totalBalance: $totalBalance" }
+            sender.sendMessage(
+                "잔고 순위 ($now)\n전체 잔고: ${totalBalance.format(2)}\n${
+                    playersWithBalance.mapIndexed { index, pair ->
+                        "${index + 1}. ${pair.first.name}: ${pair.second.format(2)}원"
+                    }.joinToString("\n")
+                }"
+            )
+            return true
+        } catch (e: Exception) {
+            log.error(e) { e.message }
+            return false
+        }
+    }
+}
--- economy/src/main/kotlin/com/sysbot32/robotmc/plugin/economy/EconomyPlugin.kt
+++ economy/src/main/kotlin/com/sysbot32/robotmc/plugin/economy/EconomyPlugin.kt
@@ -11,6 +11,7 @@ class EconomyPlugin : JavaPlugin() {
         saveConfig()
         BasicIncomeRunnable(this).runTaskTimerAsynchronously(this, 0, 20 * 20.minutes.inWholeSeconds)
         server.commandMap.register(name.lowercase(), BalanceCommand())
+        server.commandMap.register(name.lowercase(), BalanceTopCommand())
         server.commandMap.register(name.lowercase(), ReloadCommand(this))
     }
 }
