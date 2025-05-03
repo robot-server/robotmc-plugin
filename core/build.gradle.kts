dc6e015a865fe1fbde1edccc33865e37de0db7f4
Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>
2025-05-03 16:57:42 +0900

    kotlin-logging-jvm 의존성 추가
    Signed-off-by: Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 }
 
 dependencies {
+    api("io.github.oshai:kotlin-logging-jvm:7.0.7")
     api(libs.exposed.core)
     implementation(libs.exposed.jdbc)
     api(libs.exposed.kotlin.datetime)
